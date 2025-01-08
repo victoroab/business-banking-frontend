26faccea21d84ef85da7d3250c1dc607d7035763
bamidele2161 <akinyemibamidele2@gmail.com>
2025-01-08 15:49:34 +0100

    kyb badge

--- src/pages/Dashboard/Home/Home.tsx
+++ src/pages/Dashboard/Home/Home.tsx
@@ -5,14 +5,36 @@ import TransactionChart from "../../../components/Dashboard/Home/TransactionChar
 import TransactionHistory from "../../../components/Dashboard/Home/TransactionHistory";
 import Navbar from "../../../components/Navbar/Navbar";
 import { sampleData } from "../../../utils";
+import {CautionIcon} from "../../../assets/svg/PayBill"
+import {useNavigate} from "react-router-dom"
+import { useAppSelector } from "../../../hooks";
+import {
+  selectAuth
+} from "../../../store/slice/authSlice";
 
 const Dashboard = () => {
+  const navigate = useNavigate()
+  const { kybDetails } = useAppSelector(selectAuth);
+
   return (
     <div className="">
       <Navbar
         title="Good Morning, Bamidele"
         subtitle="Here’s your dashboard overview."
       />
+      { (kybDetails?.kybStatus === null || kybDetails?.kybStatus?.attestation === false) &&
+        (
+          <div className="flex justify-between bg-secColor p-3 mb-2 mx-10">
+          <div className="flex justify-between text-white w-[70%] gap-4 flex items-center">
+            <CautionIcon />
+          <p className="text-white font-workSans leading-4 font-semibold text-sm">
+          Thank you for choosing Alert as your preferred banking platform! To fully activate your account and enable secure transactions, it is mandatory to complete your Know Your Business(KYB) verification.
+          </p>
+          </div>
+            <button className="white-btn" onClick={() => navigate("/kyb/identity")}>Complete KYB</button>
+          </div>
+        )
+      }
       <div className="px-10 flex flex-col gap-10 pb-14">
         <MobileAppsCard />
         <div className="justify-between flex items-center">
