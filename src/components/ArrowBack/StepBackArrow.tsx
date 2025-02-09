4a1650e290f13a6ea0256dc9b26fb67cb4e453b2
bamidele2161 <akinyemibamidele2@gmail.com>
2025-02-09 13:34:33 +0100

    navigation fix

--- src/components/ArrowBack/StepBackArrow.tsx
+++ src/components/ArrowBack/StepBackArrow.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { BackArrowIcon } from "../../assets/svg/CustomSVGs";
 import { useAppDispatch } from "../../hooks";
 import { BackArrowProps } from "../../interfaces/Global";
@@ -7,11 +8,18 @@ const StepBackNavigation = ({
   stateCurrentStep,
 }: BackArrowProps) => {
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    stateCurrentStep === 1
+      ? navigate(-1)
+      : dispatch(setStateCurrentStep(stateCurrentStep - 1));
+  };
   return (
     <div className="flex w-full justify-center">
       <button
         className="text-secColor text-xl font-semibold text-bricolage flex gap-2 cursor-pointer items-center"
-        onClick={() => dispatch(setStateCurrentStep(stateCurrentStep - 1))}
+        onClick={handleNavigate}
       >
         {" "}
         <BackArrowIcon />
