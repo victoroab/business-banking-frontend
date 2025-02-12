85214279af73f263bc31d1ed744b4b8f11751a1e
bamidele2161 <akinyemibamidele2@gmail.com>
2025-02-12 16:45:21 +0100

    provider and package loader

--- src/pages/Dashboard/AirtimeData/NewTransaction/Package.tsx
+++ src/pages/Dashboard/AirtimeData/NewTransaction/Package.tsx
@@ -1,4 +1,5 @@
 import { ArrowRightIcon } from "../../../../assets/svg/CustomSVGs";
+import Spinner from "../../../../components/Spinner/Spinner";
 import { useAppDispatch, useAppSelector } from "../../../../hooks";
 import { useGetAllInternetBundlesQuery } from "../../../../service/billPayment";
 import {
@@ -11,7 +12,7 @@ const AirtimePackage = () => {
   const dispatch = useAppDispatch();
   const { airtimeBundlePayload } = useAppSelector(selectBillPayment);
 
-  const { data } = useGetAllInternetBundlesQuery(
+  const { data, isLoading } = useGetAllInternetBundlesQuery(
     airtimeBundlePayload?.serviceCategoryId
   );
 
@@ -35,24 +36,34 @@ const AirtimePackage = () => {
         <h3 className="text-greyColr font-semibold text-base font-bricolage leading-6">
           Categories
         </h3>
-        {data?.data.map((option: any) => (
-          <div
-            className="account-option flex flex-col cursor-pointer rounded-xl p-6 gap-4"
-            style={{ boxShadow: "0px 1px 5px 2px rgba(216, 216, 216, 0.2)" }}
-            key={option?.productId}
-            onClick={() => handleNavigate(option?.bundleCode)}
-          >
-            <div className="flex items-center justify-between">
-              <div className="flex gap-2 items-center">
-                <h2 className="text-base font-medium text-lightGreyColor m-0 font-workSans">
-                  {option?.bundleCode} for {option?.validity} NGN{" "}
-                  {option?.amount}
-                </h2>
-              </div>
-              <ArrowRightIcon />
-            </div>
-          </div>
-        ))}
+        <>
+          {isLoading ? (
+            <Spinner />
+          ) : (
+            <>
+              {data?.data.map((option: any) => (
+                <div
+                  className="account-option flex flex-col cursor-pointer rounded-xl p-6 gap-4"
+                  style={{
+                    boxShadow: "0px 1px 5px 2px rgba(216, 216, 216, 0.2)",
+                  }}
+                  key={option?.productId}
+                  onClick={() => handleNavigate(option?.bundleCode)}
+                >
+                  <div className="flex items-center justify-between">
+                    <div className="flex gap-2 items-center">
+                      <h2 className="text-base font-medium text-lightGreyColor m-0 font-workSans">
+                        {option?.bundleCode} for {option?.validity} NGN{" "}
+                        {option?.amount}
+                      </h2>
+                    </div>
+                    <ArrowRightIcon />
+                  </div>
+                </div>
+              ))}
+            </>
+          )}
+        </>
       </div>
     </div>
   );
--- src/pages/Dashboard/AirtimeData/NewTransaction/Provider.tsx
+++ src/pages/Dashboard/AirtimeData/NewTransaction/Provider.tsx
@@ -1,4 +1,5 @@
 import { ArrowRightIcon } from "../../../../assets/svg/CustomSVGs";
+import Spinner from "../../../../components/Spinner/Spinner";
 import { useAppDispatch, useAppSelector } from "../../../../hooks";
 import {
   useGetAllAirtimeProvidersQuery,
@@ -12,8 +13,10 @@ import {
 // import { dataProvider } from "../../../../utils";
 
 const Provider = () => {
-  const { data: airtime } = useGetAllAirtimeProvidersQuery();
-  const { data: bundle } = useGetAllInternetProvidersQuery();
+  const { data: airtime, isLoading: loadingAirtime } =
+    useGetAllAirtimeProvidersQuery();
+  const { data: bundle, isLoading: loadingBundle } =
+    useGetAllInternetProvidersQuery();
   const { airtimeDataAction } = useAppSelector(selectBillPayment);
   const dispatch = useAppDispatch();
 
@@ -24,7 +27,8 @@ const Provider = () => {
       : dispatch(setAirtimeDataCurrentStep(4));
   };
   const data = airtimeDataAction === "DATA" ? bundle : airtime;
-
+  const isLoading =
+    airtimeDataAction === "DATA" ? loadingBundle : loadingAirtime;
   return (
     <div className="flex flex-col gap-10">
       <div className="flex flex-col gap-4">
@@ -37,31 +41,44 @@ const Provider = () => {
       </div>
 
       <div className="flex flex-col gap-2">
-        {data?.data?.map((option: any) => (
-          <div
-            className="account-option flex flex-col cursor-pointer rounded-xl p-6 gap-4"
-            style={{ boxShadow: "0px 1px 5px 2px rgba(216, 216, 216, 0.2)" }}
-            key={option?.serviceCategoryId}
-            onClick={() =>
-              handleNavigate(option?.identifier, option?.serviceCategoryId)
-            }
-          >
-            <div className="flex items-center justify-between">
-              <div className="flex gap-2 items-center">
-                <img
-                  src={option?.logo}
-                  alt=""
-                  className="h-6 w-6 rounded-full"
-                />
-                {/* {<option.icon />} */}
-                <h2 className="text-base font-medium text-lightGreyColor m-0 font-workSans">
-                  {option?.name}
-                </h2>
-              </div>
-              <ArrowRightIcon />
-            </div>
-          </div>
-        ))}
+        <>
+          {isLoading ? (
+            <Spinner />
+          ) : (
+            <>
+              {data?.data?.map((option: any) => (
+                <div
+                  className="account-option flex flex-col cursor-pointer rounded-xl p-6 gap-4"
+                  style={{
+                    boxShadow: "0px 1px 5px 2px rgba(216, 216, 216, 0.2)",
+                  }}
+                  key={option?.serviceCategoryId}
+                  onClick={() =>
+                    handleNavigate(
+                      option?.identifier,
+                      option?.serviceCategoryId
+                    )
+                  }
+                >
+                  <div className="flex items-center justify-between">
+                    <div className="flex gap-2 items-center">
+                      <img
+                        src={option?.logo}
+                        alt=""
+                        className="h-6 w-6 rounded-full"
+                      />
+                      {/* {<option.icon />} */}
+                      <h2 className="text-base font-medium text-lightGreyColor m-0 font-workSans">
+                        {option?.name}
+                      </h2>
+                    </div>
+                    <ArrowRightIcon />
+                  </div>
+                </div>
+              ))}
+            </>
+          )}
+        </>
       </div>
     </div>
   );
